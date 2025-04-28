c50097ffaaa67b447462b49f89202943f3f41e6b
Szamansaju <szamansaju@gmail.com>
2025-04-28 14:48:53 +0600

    some changes done

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,8 +3,35 @@ import React from "react";
 const page = () => {
   return (
     <div className="dark:bg-white">
-      <div className="container flex justify-between mx-auto items-center h-screen">
-        <h1 className="text-6xl text-black text-center w-full">Login</h1>
+      <div className="container mx-auto sm:px-0 mx w-2/3 py-10">
+        <div className="grid grid-cols-1 sm:grid-cols-12 lg:grid-cols-2 xl:grid-cols-2 gap-6 justify-center items-center">
+          {/* First Card */}
+          <div className="card bg-primary text-primary-content shadow-2xl">
+            <div className="card-body text-center w-full">
+              <h2 className="card-title text-7xl">লগিন করুন !</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div className="card-actions justify-end">
+                <button className="btn">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          {/* Second Card */}
+          <div className="card bg-primary text-primary-content shadow-2xl">
+            <div className="card-body">
+              <h2 className="card-title">Card title!</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div className="card-actions justify-end">
+                <button className="btn">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MetaTags/MetaTags.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MetaTags = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MetaTags;
