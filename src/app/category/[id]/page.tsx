ea0e22aefa9010029f432cbb7e44d6920ea15a53
Szamansaju <szamansaju@gmail.com>
2025-04-29 09:47:13 +0600

    some changes done

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -6,39 +6,32 @@ export async function generateStaticParams() {
   const res = await fetch("https://backoffice.ajkal.us/news-category");
   const json = await res.json();
 
-  const categories: Category[] = json.data; // Typing it as Category array
+  const categories: Category[] = json.data;
 
   return categories.map((category) => ({
     id: category.id.toString(),
   }));
 }
 
-// This function should be async to handle params properly
 export default async function CategoryPage({ params }: CategoryPageParams) {
-  const { id } = await params; 
+  const { id } = params;
 
-  // ✅ Fetch the specific category news by id
-  const response = await fetch(
-    `https://backoffice.ajkal.us/category-news/${id}`,
-    {
-      cache: "no-store", // So it doesn't cache old data
-    }
-  );
+  const response = await fetch(`https://backoffice.ajkal.us/category-news/${id}`, {
+    cache: "no-store",
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch category news");
   }
 
   const categoryData = await response.json();
-  const singleCategoryData= categoryData.data
+  const singleCategoryData = categoryData.data;
 
   return (
     <div className="dark:bg-white">
       <div className="container mx-auto sm:px-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12 gap-6 justify-center items-center">
-          {/* Left side: Large Image */}
           <div className="col-span-12 xl:col-span-9">
-            {/* ✅ Pass data to the component */}
             <CategoryData data={singleCategoryData} />
           </div>
           <div className="col-span-12 xl:col-span-3">
