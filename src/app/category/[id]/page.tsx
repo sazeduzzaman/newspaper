4dbd3d6babb11c998e20dcba947aae59a4cb7585
Szamansaju <szamansaju@gmail.com>
2025-04-29 09:55:15 +0600

    some changes done

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,7 +1,6 @@
 import CategoryData from "@/components/CategoryData/CategoryData";
 import { Category } from "@/types/types";
 
-// Fetch all category IDs for static generation
 export async function generateStaticParams() {
   const res = await fetch("https://backoffice.ajkal.us/news-category");
   const json = await res.json();
@@ -13,16 +12,11 @@ export async function generateStaticParams() {
   }));
 }
 
-// The dynamic route component
-export default async function CategoryPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
+export default async function CategoryPage({ params }: any) {
+  const id = params?.id;
 
   const response = await fetch(`https://backoffice.ajkal.us/category-news/${id}`, {
-    cache: "no-store", // Disable caching
+    cache: "no-store",
   });
 
   if (!response.ok) {
@@ -36,12 +30,9 @@ export default async function CategoryPage({
     <div className="dark:bg-white">
       <div className="container mx-auto sm:px-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12 gap-6 justify-center items-center">
-          {/* Main content */}
           <div className="col-span-12 xl:col-span-9">
             <CategoryData data={singleCategoryData} />
           </div>
-
-          {/* Sidebar */}
           <div className="col-span-12 xl:col-span-3">
             <p className="text-black">Sidebar</p>
           </div>
