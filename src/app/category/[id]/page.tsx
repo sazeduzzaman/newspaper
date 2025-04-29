0dba82f5d524a78ba21352968459a7b54905f1d4
Szamansaju <szamansaju@gmail.com>
2025-04-29 10:01:14 +0600

    Type Error Solved & adding revalidate

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import CategoryData from "@/components/CategoryData/CategoryData";
 import { Category } from "@/lib/types/CategoryTypes";
 
@@ -13,12 +12,19 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function CategoryPage({ params }: any) {
-  const id = params?.id;
+export default async function CategoryPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
 
-  const response = await fetch(`https://backoffice.ajkal.us/category-news/${id}`, {
-    cache: "no-store",
-  });
+  const response = await fetch(
+    `https://backoffice.ajkal.us/category-news/${id}`,
+    {
+      next: { revalidate: 60 }, // ✅ cache it, refresh every 60 seconds
+    },
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch category news");
@@ -30,7 +36,7 @@ export default async function CategoryPage({ params }: any) {
   return (
     <div className="dark:bg-white">
       <div className="container mx-auto sm:px-0">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12 gap-6 justify-center items-center">
+        <div className="grid grid-cols-1 items-center justify-center gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-12">
           <div className="col-span-12 xl:col-span-9">
             <CategoryData data={singleCategoryData} />
           </div>
